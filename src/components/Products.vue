672d2b3d1c40cd2e768a7c30d8a99f109faa0c24
mauriciodesign <mauricio.a.design@gmail.com>
2020-06-16 16:07:26 -0400

    cambios en card

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -16,7 +16,12 @@
             <input type="text" id="searchBox" v-model="search"/> -->
             <div class="field">
               <p class="control has-icons-left">
-                <input class="input" type="text" placeholder="Buscar" v-model="search">
+                <input
+                  class="input"
+                  type="text"
+                  placeholder="Buscar"
+                  v-model="search"
+                />
                 <span class="icon is-small is-left">
                   <i class="mdi mdi-magnify"></i>
                 </span>
@@ -29,97 +34,111 @@
     </section>
 
     <div class="container has-padding-5">
-      <h2 class="has-text-centered is-size-5">NUEVOS LANZAMIENTOS EN LA TIENDA</h2>
-    <div class="columns is-multiline">
-      <div class="column is-4" v-for="p in computedProductList" :key="p.id">
-        <!-- Item -->
-        <div class="card has-equal-height">
-          <div class="image-card">
-            <div class="image has-spacing image">
-              <img alt="product" :src="p.data.picture">
-              <div class="product-title">
-                 <h3 class="title">{{ p.data.name }}</h3>
-                 <h4>Altars of madness </h4>
-              </div>    
+      <h2 class="has-text-centered is-size-5">
+        NUEVOS LANZAMIENTOS EN LA TIENDA
+      </h2>
+      <div class="columns is-multiline">
+        <div class="column is-4" v-for="p in computedProductList" :key="p.id">
+          <!-- Item -->
+          <div class="card has-equal-height">
+            <div class="image-card">
+              <div class="image has-spacing image">
+                <img alt="product" :src="p.data.picture" />
+                <div class="product-title">
+                  <h3 class="title">{{ p.data.name }}</h3>
+                  <h4>Altars of madness</h4>
+                </div>
+              </div>
             </div>
-          </div>
-          <div class="card-content">
-            <div class="content">
-              <p class="subtitle">$ {{ parseInt(p.data.price) }}</p>
-              <!-- <div class="has-spacing-bottom"> -->
-                <!-- <div class="has-spacing-bottom">
+            <div class="card-content">
+              <div class="content card-detail">
+                <div class="card-description">
+                  <p class="subtitle">$ {{ parseInt(p.data.price) }}</p>
+                  <!-- <div class="has-spacing-bottom"> -->
+                  <!-- <div class="has-spacing-bottom">
                   <span class="tag is-medium">tortor</span>
                   <span class="tag is-medium">suscipit</span>
                   <span class="tag is-medium">Vivamus</span>
                 </div> -->
-              <!-- </div> -->
-              <!-- <p></p> -->
-              <p>{{p.data.description}}</p>
-              <!-- <p></p> -->
-              <div class="counter">
-                <button @click="decrQty(p.id)" :disabled="p.qty === 1"><i class="mdi mdi-minus"></i></button>
-                {{ p.qty }}
-                <button @click="incrQty(p.id)"><i class="mdi mdi-plus"></i></button>
+                  <!-- </div> -->
+                  <!-- <p></p> -->
+                  <p>{{ p.data.description }}</p>
+                </div>
+                <!-- <p></p> -->
+                <div class="card-footer">
+                  <div class="counter">
+                    <button @click="decrQty(p.id)" :disabled="p.qty === 1">
+                      <i class="mdi mdi-minus"></i>
+                    </button>
+                    {{ p.qty }}
+                    <button @click="incrQty(p.id)">
+                      <i class="mdi mdi-plus"></i>
+                    </button>
+                  </div>
+                  <button
+                    @click="addToCart(p)"
+                    class="button is-pulled-right is-warning"
+                  >
+                    <i class="mdi mdi-cart"></i>
+                  </button>
+                </div>
+                <div class="is-clearfix"></div>
               </div>
-              <button @click="addToCart(p)" class="button is-pulled-right is-warning">
-                <i class="mdi mdi-cart"></i>
-              </button>
-              <div class="is-clearfix"></div>
             </div>
           </div>
         </div>
-      </div> <!-- End Item  -->
+        <!-- End Item  -->
+      </div>
+      <!--  -->
     </div>
-    <!--  -->
-  </div>
   </div>
 </template>
 
 <script>
 // import { Products } from "../services/API";
 
-import {mapState} from 'vuex'
+import { mapState } from "vuex";
 
 export default {
   components: {},
   props: {},
   data() {
     return {
-      search: '',
-    }
+      search: "",
+    };
   },
   methods: {
     incrQty(id) {
-      let idx = this.products.map(p => p.id).indexOf(id)
-      this.products[idx].qty++
+      let idx = this.products.map((p) => p.id).indexOf(id);
+      this.products[idx].qty++;
     },
     decrQty(id) {
-      let idx = this.products.map(p => p.id).indexOf(id)
+      let idx = this.products.map((p) => p.id).indexOf(id);
       // console.log('decr on ', this.products[idx])
-      if(this.products[idx].qty > 1) {
-        this.products[idx].qty--
+      if (this.products[idx].qty > 1) {
+        this.products[idx].qty--;
       }
     },
     addToCart(product) {
       // push a copy of product to the cart
-      let p = JSON.parse(JSON.stringify(product))
-      this.$store.dispatch('addToCart', p)
+      let p = JSON.parse(JSON.stringify(product));
+      this.$store.dispatch("addToCart", p);
       // reset qty
-      product.qty = 1
+      product.qty = 1;
     },
   },
   computed: {
-    ...mapState(['products']),
+    ...mapState(["products"]),
     computedProductList() {
       console.log(this.products);
-      return this.products.filter(p => {
-        return p.data.name.toLowerCase().includes(this.search.toLowerCase())
-      })
-    }
+      return this.products.filter((p) => {
+        return p.data.name.toLowerCase().includes(this.search.toLowerCase());
+      });
+    },
   },
   watch: {},
   created() {
-    this.$store.dispatch('getProducts')
+    this.$store.dispatch("getProducts");
     // Pull products from Product service
     // Products.all().then(response => {
     //   response.data.forEach(p => {
@@ -131,8 +150,8 @@ export default {
     //   alert(error.message)
     // })
   },
-  mounted() {}
-}
+  mounted() {},
+};
 </script>
 
 <style lang="scss" scoped>
@@ -147,58 +166,63 @@ export default {
   }
 }
 .header {
-    display: flex;
-    justify-content: space-between;
-    align-items: flex-end;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
 }
-.hero{
-  background: url('/img/header.png') no-repeat bottom !important;
+.hero {
+  background: url("/img/header.png") no-repeat bottom !important;
   background-size: cover !important;
   height: 290px !important;
 }
-.hero-body{
+.hero-body {
   padding: 4rem 1.5rem;
-      display: flex;
-    align-items: flex-end;
+  display: flex;
+  align-items: flex-end;
 }
-.input{
+.input {
   padding: 10px 10px 10px 3.5rem !important;
   height: auto;
   border-radius: 30px;
 }
-.search{
+.search {
   width: 22%;
 }
-.icon i{
-    font-size: 1.8rem;
-    color: #a7a5a5;
+.icon i {
+  font-size: 1.8rem;
+  color: #a7a5a5;
 }
-.icon{
-    width: 3.5rem !important;
-    height: 3rem !important;
+.icon {
+  width: 3.5rem !important;
+  height: 3rem !important;
 }
-.product-title{
-    background: rgba(0,0,0,0.6);
-    position: absolute;
-    bottom: 0px;
-    padding: 20px;
-    width: 100%;
-    .title{
-      font-weight: bold;
-      color: #fff;
-      margin: 0;
-    }
-    h4{
-      color: #fff;
-      font-size: 1.5em;
-      font-weight: normal;
-    }
+.product-title {
+  background: rgba(0, 0, 0, 0.6);
+  position: absolute;
+  bottom: 0px;
+  padding: 20px;
+  width: 100%;
+  .title {
+    font-weight: bold;
+    color: #fff;
+    margin: 0;
+  }
+  h4 {
+    color: #fff;
+    font-size: 1.5em;
+    font-weight: normal;
+  }
 }
-.image img{
+.image img {
   border-radius: 20px 20px 0 0;
 }
-.card{
-  border-radius: 20px ;
+.card {
+  border-radius: 20px;
   height: 100%;
+}
+.card-detail{
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
 }
 </style>
