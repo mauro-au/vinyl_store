60b0034029ee18a788d2cb6d9764c889a3b64fdd
mauriciodesign <mauricio.a.design@gmail.com>
2020-06-22 06:19:43 -0400

    Maquetado en vista create y se agrega favicon

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,69 +1,50 @@
 <template>
   <div>
-    <h1>Crear Producto</h1>
-    <section class="section">
+    <section class="section mt-5">
       <div class="container">
+        <div class="columns is-centered">
+          <div class="column is-two-thirds">
+        <h2 class="title is-1 has-text-centered create__title">Crear Producto</h2>
         <form class="login-container">
-          <div class="control">
-            <label>Nombre Banda</label>
-            <input
-              class="input"
-              type="text"
-              placeholder="Normal input"
-              v-model="name"
-            />
+          <div class="field">
+            <label>Nombre de Banda</label>
+            <input class="input is-medium" type="text" placeholder="Nombre de Banda" v-model="name"/>
           </div>
-          <div class="control">
+          <div class="field">
             <label>Album</label>
-            <input
-              class="input"
-              type="text"
-              placeholder="Normal input"
-              v-model="album"
-            />
+            <input class="input is-medium" type="text" placeholder="Album" v-model="album"/>
           </div>
-          <div class="control">
+          <div class="field">
             <label>Imagen</label>
-            <input
-              class="input"
-              type="text"
-              placeholder="Normal input"
-              v-model="picture"
-            />
+            <input class="input is-medium" type="text" placeholder="Imagen" v-model="picture"/>
           </div>
-          <div class="control">
+          <div class="field">
             <label>Precio</label>
-            <input
-              class="input"
-              type="number"
-              placeholder="Precio"
-              v-model="price"
-            />
+            <input class="input is-medium" type="number" placeholder="Precio" v-model="price"/>
           </div>
-          <div class="control">
+          <div class="field">
             <label>Descripción</label>
-            <textarea
-              class="textarea"
-              placeholder="Descripción"
-              v-model="description"
-            ></textarea>
+            <textarea class="textarea is-medium" placeholder="Descripción" v-model="description"></textarea>
           </div>
 
-          <button @click.prevent="createProduct">Añadir</button>
-          <button v-if="edit" @click="updateProduct(id)">Actualizar</button>
+          <button class="button is-success" @click.prevent="createProduct">Añadir</button>
+          <button class="button is-info mx-4" v-if="edit" @click="updateProduct(id)">Actualizar</button>
         </form>
+      </div>
+      </div>
       </div>
     </section>
     <section>
       <div class="container">
-        <h1>Listar Productos</h1>
+        <h2 class="title is-1 has-text-centered create__title">Lista de Productos</h2>
+        <div class="columns is-centered">
         <table class="table is-responsive">
           <thead>
             <tr>
-              <th>Nombre Banda</th>
+              <th>Banda</th>
               <th>Album</th>
               <th>Precio</th>
-              <th>Descripción</th>
+              <th class="description-th">Descripción</th>
               <th>Imagen</th>
               <th colspan="2" class="action">Acción</th>
             </tr>
@@ -72,8 +53,8 @@
             <tr v-for="p in computedProductList" :key="p.id">
               <td>{{ p.data.name }}</td>
               <td>{{ p.data.album }}</td>
-              <td>{{ p.data.price }}</td>
-              <td>{{ p.data.description }}</td>
+              <td>${{ p.data.price }}</td>
+              <td>{{ p.data.description.substring(0,100) }}...</td>
               <td><img alt="product logo" :src="p.data.picture"></td>
               <td>
                 <button class="button btn-success" @click="editProduct(p.id)">Editar</button>
@@ -86,6 +67,7 @@
             </tr>
           </tbody>
         </table>
+      </div>
       </div>
     </section>
   </div>
@@ -112,7 +94,7 @@ export default {
         album: this.album,
         picture: this.picture,
         price: this.price,
-        description: this.description,
+        description: this.description
       };
       axios
         .post(
@@ -120,82 +102,111 @@ export default {
           create,
           { headers: { "content-type": "application/json" } }
         )
-        .then((response) => {
+        .then(response => {
           console.log(response);
           this.$store.dispatch("getProducts");
         })
-        .catch((error) => {
+        .catch(error => {
           console.log(error);
         });
     },
     deleteProduct(id) {
       let confirmation = confirm("Seguro?");
       if (confirmation) {
-          axios.delete(`https://us-central1-tddg3-ca51a.cloudfunctions.net/products/product/${id}`, 
-            { headers: { "content-type": "application/json" }}
-        )
-        .then(() => {
+        axios
+          .delete(
+            `https://us-central1-tddg3-ca51a.cloudfunctions.net/products/product/${id}`,
+            { headers: { "content-type": "application/json" } }
+          )
+          .then(() => {
             alert("Produco Eliminado");
             this.$store.dispatch("getProducts");
-        });
+          });
       }
     },
-    editProduct(id){
-        this.updateEdit()
-        this.findProduct(id)
+    editProduct(id) {
+      this.updateEdit();
+      this.findProduct(id);
     },
-    findProduct(id){
-        axios.get(`https://us-central1-tddg3-ca51a.cloudfunctions.net/products/product/${id}`, 
-            { headers: { "content-type": "application/json" }}
+    findProduct(id) {
+      axios
+        .get(
+          `https://us-central1-tddg3-ca51a.cloudfunctions.net/products/product/${id}`,
+          { headers: { "content-type": "application/json" } }
         )
-        .then((response) =>{
-            this.name = response.data.name
-            this.album = response.data.album
-            this.picture = response.data.picture
-            this.price = response.data.price
-            this.description = response.data.description
-            this.id = id
-        })
+        .then(response => {
+          this.name = response.data.name;
+          this.album = response.data.album;
+          this.picture = response.data.picture;
+          this.price = response.data.price;
+          this.description = response.data.description;
+          this.id = id;
+        });
     },
-    updateProduct(id){
+    updateProduct(id) {
       let create = {
         name: this.name,
         album: this.album,
         picture: this.picture,
         price: this.price,
-        description: this.description,
+        description: this.description
       };
-      axios.put(`https://us-central1-tddg3-ca51a.cloudfunctions.net/products/product/${id}`, create,
+      axios
+        .put(
+          `https://us-central1-tddg3-ca51a.cloudfunctions.net/products/product/${id}`,
+          create,
           { headers: { "content-type": "application/json" } }
         )
         .then(() => {
-          this.name = ''
-          this.album = ''
-          this.price = ''
-          this.picture = ''
-          this.description = ''
-          this.id = ''
+          this.name = "";
+          this.album = "";
+          this.price = "";
+          this.picture = "";
+          this.description = "";
+          this.id = "";
           this.$store.dispatch("getProducts");
         })
-        .catch((error) => {
+        .catch(error => {
           console.log(error);
         });
-    },
+    }
   },
   computed: {
     ...mapState(["products", "edit"]),
     computedProductList() {
       return this.products;
-    },
+    }
   },
   mounted() {
     this.$store.dispatch("getProducts");
-  },
+  }
 };
 </script>
 
 <style lang="scss" scoped>
 
+.create__title{
+    font-weight: bold;
+    margin: 2.5rem 0 2rem 0;
+}
+.login-container{
+  font-size: 1.2rem;
+  font-weight: bold;
+}
+th, td{
+  width: auto;
+  padding: 20px;
+}
+
+table{
+    width: 80vw;
+    border-radius: 10px;
+    margin-top: 20px;
+}
+img{
+    width: 20vh;
+    border-radius: 10px;
+}
 @media screen and (max-width: 800px) {
   .is-responsive {
     width: 100%;
@@ -268,5 +279,4 @@ export default {
     border-width: 1px;
   }
 }
-
 </style>
